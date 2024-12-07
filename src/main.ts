8e83e3f859f56f0ca06c74913ce1a3b494d879fa
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-12-07 05:28:25 -0500

    frontend domain updated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://amazon-front-gamma.vercel.app',
     credentials: true,
   });
 
